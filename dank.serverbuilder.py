422b06796368d66e4943765a9b98b6ca6f6f7ffe
SirDankenstien <52797753+SirDankenstien@users.noreply.github.com>
2021-10-09 15:54:46 +0530

    Update dank.serverbuilder.py
    better looking output :smile:

--- dank.serverbuilder.py
+++ dank.serverbuilder.py
@@ -77,7 +77,7 @@ except:
 
 project = "dank.serverbuilder"
 current_version = 1.4
-print(f"\n{white}> {magenta}Version{white}: {current_version}")
+print(f"\n  {white}> {magenta}Version{white}: {current_version}")
 
 Success = False
 while not Success:
@@ -89,24 +89,24 @@ while not Success:
             latest_version = float(latest_version)
         Success = True
     except:
-        wait = input(f"\n{white}> {red}Failed to check for an update! Make sure you are connected to the Internet! Press {white}Enter {red}to try again.")
+        wait = input(f"\n  {white}> {red}Failed to check for an update! Make sure you are connected to the Internet! Press {white}Enter {red}to try again.")
 
 if version.parse(str(latest_version)) > version.parse(str(current_version)):
 
-    choice = str(input(f"\n{white}> {magenta}Update Found{white}: {latest_version}\n\n{white}> {magenta}Download latest version? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
+    choice = str(input(f"\n  {white}> {magenta}Update Found{white}: {latest_version}\n\n  {white}> {magenta}Download latest version? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
     if choice == "y":
-        print(f"\n{white}> {magenta}Downloading {white}{project}-latest.exe{magenta}...")
+        print(f"\n  {white}> {magenta}Downloading {white}{project}-latest.exe{magenta}...")
         data = requests.get(f"https://github.com/SirDankenstien/{project}/blob/main/{project}.exe?raw=true", allow_redirects=True).content
         open(f"{project}-latest.exe","wb").write(data)
         data = None
-        print(f"\n{white}> {magenta}Downloaded!\n\n{magenta}Terminating in 5s...")
+        print(f"\n  {white}> {magenta}Downloaded!\n\n{magenta}Terminating in 5s...")
         time.sleep(5)
         sys.exit()
 
 #elif latest_version == current_version:
-#    print(f"\n{white}> {magenta}Latest Version!")
+#    print(f"\n  {white}> {magenta}Latest Version!")
 #else:
-#    print(f"\n{white}> {magenta}Development Version!")
+#    print(f"\n  {white}> {magenta}Development Version!")
 
 '''
 
@@ -116,17 +116,17 @@ Success = False
 while not Success:
     try:
         response = requests.get("https://papermc.io/api/v2/projects/paper/").json()
-        available_versions = f"\n{white}> {magenta}Available Paper Versions{white}: {magenta}" + str(response['versions']).replace("[","").replace("]","").replace("\'","").replace(".",f"{white}.{magenta}").replace(",",f"{white},{magenta}")
+        available_versions = f"\n  {white}> {magenta}Available Paper Versions{white}: {magenta}" + str(response['versions']).replace("[","").replace("]","").replace("\'","").replace(".",f"{white}.{magenta}").replace(",",f"{white},{magenta}")
         print(available_versions)
         available_versions = available_versions.replace(f"{white}","").replace(f"{magenta}","")
         Success = True
     except:
-        wait = input(f"\n{white}> {red}Failed to get paper versions! Make sure you are connected to the Internet! Press {white}Enter {red}to try again.")
+        wait = input(f"\n  {white}> {red}Failed to get paper versions! Make sure you are connected to the Internet! Press {white}Enter {red}to try again.")
 
 # input
 
-name = str(input(f"\n{white}> {magenta}Server Name{white}: {magenta}"))
-version = str(input(f"\n{white}> {magenta}Version{white}: {magenta}"))
+name = str(input(f"\n  {white}> {magenta}Server Name{white}: {magenta}"))
+version = str(input(f"\n  {white}> {magenta}Version{white}: {magenta}"))
 
 # check if version is available
 
@@ -142,15 +142,15 @@ def version_check():
 version_check()
 
 while not version_available:
-    print(f"\n{white}> {red}That version is not supported{white}!")
-    version = str(input(f"\n{white}> {magenta}Version{white}: {magenta}"))
+    print(f"\n  {white}> {red}That version is not supported{white}!")
+    version = str(input(f"\n  {white}> {magenta}Version{white}: {magenta}"))
     version_check()
-ram = str(input(f"\n{white}> {magenta}RAM in MB {white}[ {magenta}Above 512 {white}]: {magenta}"))
-offline = str(input(f"\n{white}> {magenta}Allow Cracked Players {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}"))
+ram = str(input(f"\n  {white}> {magenta}RAM in MB {white}[ {magenta}Above 512 {white}]: {magenta}"))
+offline = str(input(f"\n  {white}> {magenta}Allow Cracked Players {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}"))
 
-print(f"\n{white}> {magenta}The following step is {white}required {magenta}to run a minecraft paper server of version 1{white}.{magenta}17 and above{white}!")
+print(f"\n  {white}> {magenta}The following step is {white}required {magenta}to run a minecraft paper server of version 1{white}.{magenta}17 and above{white}!")
 print(f"{white}> {magenta}If you do not know or are unsure, type {white}\"{magenta}y{white}\"")
-download_jdk = str(input(f"{white}> {magenta}Do you want to download {white}OpenJDK-16{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
+download_jdk = str(input(f"  {white}> {magenta}Do you want to download {white}OpenJDK-16{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
 
 read_me = f'''
 
@@ -170,20 +170,20 @@ read_me_colored = read_me.replace(":",f"{white}:").replace("+",f"{white}+").repl
 os.system('cls')
 sys.stdout.write(aligner(read_me, read_me_colored))
 
-print(f"\n{white}> {magenta}Prefarably use {white}port forwarding {magenta}over {white}noip.com {magenta}and {white}noip.com {magenta}over {white}ngrok")
-print(f"{white}> {magenta}Note{white}: {magenta}there is a monthly manual renewal {white}({magenta}free{white}) {magenta}for each domain used in {white}noip.com")
-print(f"{white}> Port forwarding {magenta}is not hard at all! A tutorial has been provided at the end of this script{white}.")
-print(f"{white}> {magenta}If you do wish to {white}port forward {magenta}you can skip {white}noip.com {magenta}and {white}ngrok.")
+print(f"\n  {white}> {magenta}Prefarably use {white}port forwarding {magenta}over {white}noip.com {magenta}and {white}noip.com {magenta}over {white}ngrok")
+print(f"  {white}> {magenta}Note{white}: {magenta}there is a monthly manual renewal {white}({magenta}free{white}) {magenta}for each domain used in {white}noip.com")
+print(f"  {white}> Port forwarding {magenta}is not hard at all! A tutorial has been provided at the end of this script{white}.")
+print(f"  {white}> {magenta}If you do wish to {white}port forward {magenta}you can skip {white}noip.com {magenta}and {white}ngrok.")
 
-print(f"\n{white}> {magenta}The following step is {white}not required {magenta}to host a minecraft server if you are either {white}port forwarding {magenta}or using {white}ngrok")
-open_noip = str(input(f"{white}> {magenta}Do you want to open {white}noip.com{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
+print(f"\n  {white}> {magenta}The following step is {white}not required {magenta}to host a minecraft server if you are either {white}port forwarding {magenta}or using {white}ngrok")
+open_noip = str(input(f"  {white}> {magenta}Do you want to open {white}noip.com{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
 
 if open_noip == "y":
-    print(f"\n{white}> {magenta}Opening {white}noip.com")
+    print(f"\n  {white}> {magenta}Opening {white}noip.com")
     web.open_new_tab("https://www.noip.com/download?page=win")
 
-print(f"\n{white}> {magenta}The following step is {white}not required {magenta}to host a minecraft server if you are either {white}port forwarding {magenta}or using {white}noip.com's {magenta}Dynamic DNS Update Client{white}.")
-download_ngrok = str(input(f"{white}> {magenta}Do you want to download {white}ngrok{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
+print(f"\n  {white}> {magenta}The following step is {white}not required {magenta}to host a minecraft server if you are either {white}port forwarding {magenta}or using {white}noip.com's {magenta}Dynamic DNS Update Client{white}.")
+download_ngrok = str(input(f"  {white}> {magenta}Do you want to download {white}ngrok{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
 
 # go to workspace
 
@@ -192,7 +192,7 @@ original_name = name
 try:
     os.mkdir(name)
 except:
-    print(f"\n{white}> {red}The folder {white}{name} {red}already exists thus can't be created{white}! {red}Creating {white}{name} 2{red}...")
+    print(f"\n  {white}> {red}The folder {white}{name} {red}already exists thus can't be created{white}! {red}Creating {white}{name} 2{red}...")
     name+= " 2"
     os.mkdir(name)
 
@@ -206,7 +206,7 @@ zipfile.ZipFile(f"{filepath_temp}\DankServerBuilder.zip", 'r').extractall()
 # begin download phase
 
 os.system('cls')
-print(f"\n{white}> {magenta}Preparing Downloads{white}...")
+print(f"\n  {white}> {magenta}Preparing Downloads{white}...")
 
 to_download_urls = []
 to_download_filenames = []
@@ -215,7 +215,7 @@ def downloader(url, filename):
     data = requests.get(url, allow_redirects=True).content
     open(filename,"wb").write(data)
     data = ""
-    print(f"\n{white}> {magenta}Completed {white}{filename}{magenta}!")
+    print(f"\n  {white}> {magenta}Completed {white}{filename}{magenta}!")
 
 # EssentialsX.jar
 
@@ -308,7 +308,7 @@ if download_jdk == "y":
 
 # begin downloads
 
-print(f"\n{white}> {magenta}Starting Multiple Downloads{white}... [ {magenta}this might take a minute {white}]")
+print(f"\n  {white}> {magenta}Starting Multiple Downloads{white}... [ {magenta}this might take a minute {white}]")
 
 start_time = time.time()
 
@@ -326,8 +326,8 @@ futures.clear()
 time_taken = ( time.time() - start_time ) / 60
 
 os.system('cls')
-print(f"\n{white}> {magenta}Finished All Downloads in {white}{{0:.1f}} {magenta}minutes{white}!".format(time_taken))
-print(f"\n{white}> {magenta}Updating {white}server.properties")
+print(f"\n  {white}> {magenta}Finished All Downloads in {white}{{0:.1f}} {magenta}minutes{white}!".format(time_taken))
+print(f"\n  {white}> {magenta}Updating {white}server.properties")
 
 # updating server.properties
 
@@ -338,7 +338,7 @@ if offline == "y":
 
 open("server.properties","w").write(data)
 
-print(f"\n{white}> {magenta}Creating batch scripts...")
+print(f"\n  {white}> {magenta}Creating batch scripts...")
 
 # Creating .cmd(s)
 
@@ -384,42 +384,42 @@ if download_ngrok == "y":
     zipfile.ZipFile("ngrok.zip", 'r').extractall()
     os.remove("ngrok.zip")
 
-    print(f"\n{white}> {magenta}Go to Authentication {white}> {magenta}Your Authtoken and click copy, then paste it here{white}.")
-    print(f"{white}> {magenta}Opening {white}ngrok.com {magenta}in {white}15 {magenta}seconds.")
+    print(f"\n  {white}> {magenta}Go to Authentication {white}> {magenta}Your Authtoken and click copy, then paste it here{white}.")
+    print(f"  {white}> {magenta}Opening {white}ngrok.com {magenta}in {white}15 {magenta}seconds.")
 
     time.sleep(15)
     web.open_new_tab("https://dashboard.ngrok.com/auth/your-authtoken")
-    ngrok_token = str(input(f"\n{white}> {magenta}Ngrok Auth Token{white}: {magenta}"))
+    ngrok_token = str(input(f"\n  {white}> {magenta}Ngrok Auth Token{white}: {magenta}"))
     print()
     os.system(f"ngrok.exe authtoken {ngrok_token}")
 
 if download_jdk == "y":
 
-    print(f"\n{white}> {magenta}Starting {white}OpenJDK-16.msi")
+    print(f"\n  {white}> {magenta}Starting {white}OpenJDK-16.msi")
     os.system(f"start {installer_filename}")
 
-    temp = str(input(f"\n{white}> {magenta}Once you have sucessfully installed and closed {white}OpenJDK-16 {magenta}hit {white}[ {magenta}enter {white}] {magenta}to delete the installer{white}: {magenta}"))
+    temp = str(input(f"\n  {white}> {magenta}Once you have sucessfully installed and closed {white}OpenJDK-16 {magenta}hit {white}[ {magenta}enter {white}] {magenta}to delete the installer{white}: {magenta}"))
     os.remove(installer_filename)
 
 os.system('cls')
 sys.stdout.write(aligner(read_me, read_me_colored))
 
 if download_ngrok == "y":
-    print(f"\n{white}> {magenta}To start your server, run {white}start_server_and_ngrok.cmd")
-    print(f"{white}> {magenta}To allow players to connect to your server over the internet, {white}ngrok {magenta}must be running. This is for if you do not wish to {white}port forward {magenta}or use {white}noip.com's {magenta}Dynamic DNS Update Client{white}.")
-    print(f"{white}> {magenta}Your servers IP is shown in the ngrok window, it looks something like this {white}> {magenta}0.tcp.ngrok.io:00000 {white}< {magenta}last 5 digits will be random.")
+    print(f"\n  {white}> {magenta}To start your server, run {white}start_server_and_ngrok.cmd")
+    print(f"  {white}> {magenta}To allow players to connect to your server over the internet, {white}ngrok {magenta}must be running. This is for if you do not wish to {white}port forward {magenta}or use {white}noip.com's {magenta}Dynamic DNS Update Client{white}.")
+    print(f"  {white}> {magenta}Your servers IP is shown in the ngrok window, it looks something like this {white}> {magenta}0.tcp.ngrok.io:00000 {white}< {magenta}last 5 digits will be random.")
 else:
-    print(f"\n{white}> {magenta}To start your server, run {white}start_server.cmd")
+    print(f"\n  {white}> {magenta}To start your server, run {white}start_server.cmd")
 
 if open_noip == "y":
-    print(f"{white}> {magenta}To allow players to connect to your server over the internet, follow this tutorial on using {white}noip{magenta}. This is for if you do not wish to {white}port forward {magenta}or use {white}ngrok.")
-    open_youtube = str(input(f"{white}> {magenta}Do you want to open {white}noip tutorial {magenta}on {white}youtube{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
+    print(f"  {white}> {magenta}To allow players to connect to your server over the internet, follow this tutorial on using {white}noip{magenta}. This is for if you do not wish to {white}port forward {magenta}or use {white}ngrok.")
+    open_youtube = str(input(f"  {white}> {magenta}Do you want to open {white}noip tutorial {magenta}on {white}youtube{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
 
     if open_youtube == "y":
         web.open_new_tab("https://youtu.be/L9tbsra48c0")
 
-print(f"\n{white}> {magenta}To allow players to connect to your server over the internet, follow this tutorial on {white}port forwarding. {magenta}This is for if you do not wish to use {white}noip.com {magenta}or {white}ngrok.")
-open_youtube = str(input(f"{white}> {magenta}Do you want to open {white}port forwarding tutorial {magenta}on {white}youtube{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
+print(f"\n  {white}> {magenta}To allow players to connect to your server over the internet, follow this tutorial on {white}port forwarding. {magenta}This is for if you do not wish to use {white}noip.com {magenta}or {white}ngrok.")
+open_youtube = str(input(f"  {white}> {magenta}Do you want to open {white}port forwarding tutorial {magenta}on {white}youtube{magenta}? {white}[ {magenta}y {white}/ {magenta}n {white}]: {magenta}")).lower()
 
 if open_youtube == "y":
     web.open_new_tab("https://youtu.be/X75GbRaGzu8")
